725cde2d13f39b0f5664d0f73b9191c03dca49d3
Sviatoslav K <s_korostylev@inbox.ru>
2025-08-04 12:05:22 +0300

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
@@ -5,4 +6,8 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   base: "/my-vite-project/",
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
 });
