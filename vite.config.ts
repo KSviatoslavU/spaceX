a1b97c2de4f408b46447dea469a6c00ff8842e24
Sviatoslav K <s_korostylev@inbox.ru>
2025-09-21 13:05:15 +0300

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/my-vite-project/",
+  base: "/spaceX/",
   plugins: [react()],
   test: {
     globals: true,
